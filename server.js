ed7949130b12fb0923ba80a892db9ed8e1ddfb63
Jambalsuren0012 <naruto_0212@yahoo.com>
2023-03-02 15:18:06 +0800

    server

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3307,
+  user: "root",
+  password: "",
+  database: "azure_db",
+});
+
 
 const usersRoute = require("./routes/users");
 const categoriesRoute = require("./routes/categories");
+
 
 const port = 8000;
 
@@ -13,6 +24,28 @@ server.use(express.json());
 
 server.use("/users", usersRoute);
 server.use("/categories", categoriesRoute);
+
+server.get("/", async (req, res) => {
+  connection.query("SELECT * FROM azure_user",(err,result)=>{
+    if (err){
+      res.status(400).json({message:err.message});
+      return;
+    };
+    res.status(200).json({message:"Amjilttai", result});
+  });
+});
+
+server.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  connection.query(`SELECT * FROM azure_user WHERE aid=${id}`,(err,result)=>{
+    if (err){
+      res.status(400).json({message:err.message});
+      return;
+    };
+    res.status(200).json({message:"Amjilttai", result});
+  });
+});
+
 
 server.listen(port, () => {
   console.log(`Server is running at ${port}`);
